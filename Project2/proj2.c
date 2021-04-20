0534fb724e7818d013d4f16bb65f7cf14c74e6f8
Marian Keszi <marian.keszi@gmail.com>
2021-04-20 21:03:56 +0200

    Implemented: - semaphores init - semaphores destroy ! Semaphores init prints error !

--- Project2/proj2.c
+++ Project2/proj2.c
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 int NE; // The number of elves to be generated
 int NR; // The number of reindeers to be generated
@@ -19,9 +21,13 @@ sem_t *santaSem = NULL; // Santa waits on santaSem until either an elf or a rein
 sem_t *reindeerSem = NULL; // The reindeers waits on reindeerSem until Santa signals them to enter thepaddock and get hitched
 sem_t *elfTex = NULL; // Used to prevent additional elves from entering while three elves are being helped
 sem_t *mutex = NULL; // Protects the elves and raindeers
+sem_t *fileWrite = NULL; // Protection for writing into file
 
 FILE **file = NULL; // Variable for the proj2.out
 int fileID = 0; // ID variable for the shared memory usage
+
+int rowID = 0;
+int *row = NULL;
 
 /*Function for argument corectness check*/
 int argCheck(int argc, char **argv)
@@ -113,6 +119,18 @@ int initMemory()
         fprintf(stderr, "Error occured while attaching the shared memory segment\n");
         return 1;
     }
+    
+    if ((rowID = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666)) == -1)
+    {
+        fprintf(stderr, "Shared memory segment could not be created\n");
+        return 1;
+    }
+
+    if ((row = shmat(rowID, NULL, 0)) == NULL)
+    {
+        fprintf(stderr, "Error occured while attaching the shared memory segment\n");
+        return 1;
+    }
 
     return 0; // If the process of shared memory initialization was successfull function returns 0
 }
@@ -123,6 +141,47 @@ void destMemory(){
     {
         shmctl(fileID, IPC_RMID, NULL);
     }
+    
+    if(row != NULL)
+    {
+        shmctl(rowID, IPC_RMID, NULL);
+    }
+}
+
+void destSemaphore(sem_t *sem, char *name)
+{
+    sem_close(sem);
+    sem_unlink(name);
+}
+
+void destSemaphores()
+{
+    if(mutex != NULL)
+        destSemaphore(mutex, "/mutex_sem");
+    if(santaSem != NULL)
+        destSemaphore(santaSem, "/santa_sem");
+    if(elfTex != NULL)
+        destSemaphore(elfTex, "/elfTex");
+    if(reindeerSem != NULL)
+        destSemaphore(reindeerSem, "/reindeer_sem");
+    if(fileWrite != NULL)
+        destSemaphore(fileWrite, "/fileWrite");
+}
+
+void santaWrite(char *write, int action, int wait, int numb)
+{
+    sem_wait(fileWrite);
+    if(numb == 1)
+        fprintf(*file, write, action);
+    else
+        fprintf(*file, write, action, wait);
+    (*row)++; //raising the action number
+    sem_post(fileWrite);
+}
+
+void santa()
+{
+    santaWrite("%d: Santa: going to sleep\n", *row, 0, 1);
 }
 
 int main(int argc, char **argv)
@@ -152,6 +211,8 @@ int main(int argc, char **argv)
         return 1;
     }
     
+    *row = 1;
+
     *file = fopen("proj2.out", "w"); // Opening the output file for writing
     /*Control check if file was opened successfully, if wan't returns 1*/
     if(file == NULL)
@@ -160,6 +221,39 @@ int main(int argc, char **argv)
         return 1;
     }
     setbuf(*file, NULL); // Sets the file buffer to NULL
+
+    if((mutex = sem_open("/mutex_sem", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED
+       || (santaSem = sem_open("/santa_sem", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED
+       || (elfTex = sem_open("/elfTex", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED
+       || (reindeerSem = sem_open("/reindeer_sem", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED
+       || (fileWrite = sem_open("/fileWrite", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED)
+    {
+        fprintf(stderr, "Error while creating semaphores\n");
+        destSemaphores();
+        destMemory();
+        fclose(*file);
+        return 1;
+    }
+
+    pid_t main_process = fork(); // Forking the main_process
+
+    if(main_process == 0) //child = bus process
+    {
+        santa();
+        exit(0);
+    }
+    else if(main_process > 0)
+    {
+        //parent
+    }
+    else // Error check
+    {
+        fprintf(stderr, "Error while creating process\n");
+        destSemaphores();
+        destMemory();
+        fclose(*file);
+        return 1;
+    }
 
     return 0; // Correctly ending the programm with no errors ocured
 }
